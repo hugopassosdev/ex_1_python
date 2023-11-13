eba7cf3b3b1b937cefab6b8aac60a7ba5390ded6
Hugo Passos <hugopassosdev@outlook.com>
2023-11-13 16:55:22 -0300

    commit inicial

---
+++ ex_1_for.py
@@ -0,0 +1,13 @@
+# Instruções do projeto
+# Precisamos imprimir um número para cada andar de um hotel de 20 andares. Porém, o dono do hotel é supersticioso e optou por não ter um 13ro andar.
+# Escreva um código que imprima todos os números exceto o número 13.
+# Escreva mais um código que resolva o mesmo problema, mas dessa vez usando o laço de repetição 'while'.
+# Como desafio, imprima eles em ordem decrescente (20, 19, 18...)
+
+NUMERO = 20
+for i in range(NUMERO,0,-1):
+    if (i == 13):
+        continue
+    if (i == 0):
+        break
+    print(f"Andar {i}")
---
+++ ex_1_while.py
@@ -0,0 +1,14 @@
+# Instruções do projeto
+# Precisamos imprimir um número para cada andar de um hotel de 20 andares. Porém, o dono do hotel é supersticioso e optou por não ter um 13ro andar.
+# Escreva um código que imprima todos os números exceto o número 13.
+# Escreva mais um código que resolva o mesmo problema, mas dessa vez usando o laço de repetição 'while'.
+# Como desafio, imprima eles em ordem decrescente (20, 19, 18...)
+
+numero = 20
+while(numero <=20 and numero > 0):
+    if (numero == 13):
+        continue
+    if (numero == 0):
+        break
+    print(f"Andar {numero}")
+    numero = numero - 1
